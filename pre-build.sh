460e39f2d6abb2b71428463ae98205d002269cd1
xor <48882580+two-bit-xor@users.noreply.github.com>
2019-09-12 22:51:10 +1200

    变量导出

--- pre-build.sh
+++ pre-build.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-export CMAKE_C_COMPILER=/usr/local/Cellar/gcc/8.3.0_2/bin/gcc-8
-export CMAKE_CXX_COMPILER=/usr/local/Cellar/gcc/8.3.0_2/bin/g++-8
+echo "CMAKE_C_COMPLIER=${CMAKE_C_COMPILER}"
+echo "CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}"
 
 export CJSON_VERSION=1.7.11
-export ZF_LOG_VERSION=0.4.0
 export LWS_VERSION=3.1-stable
 
 export ROOT_WORKING_DIR=$(pwd)
@@ -25,8 +24,10 @@ rm -rf build/lws && \
         -DLWS_WITH_STATIC=ON \
         -DLWS_WITH_ZIP_FOPS=OFF \
         -DLWS_WITH_ZLIB=OFF \
+        -DCMAKE_BUILD_TYPE=DEBUG \
         -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl \
         -DCMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE=/usr/local/opt/openssl \
+        -DLWS_SSL_SERVER_WITH_ECDH_CERT=1 \
         -DOPENSSL_LIBRARIES="/usr/local/opt/openssl/lib/libcrypto.dylib;/usr/local/opt/openssl/lib/libssl.dylib" \
         -DOPENSSL_INCLUDE_DIRS=/usr/local/opt/openssl/include && \
     make install
@@ -47,15 +48,3 @@ wget https://github.com/DaveGamble/cJSON/archive/v${CJSON_VERSION}.tar.gz -O /tm
         -DCMAKE_INSTALL_PREFIX=${ROOT_WORKING_DIR}/install/cJSON/${CJSON_VERSION} && \
     make install
 
-cd ${ROOT_WORKING_DIR}
-wget https://github.com/wonder-mice/zf_log/archive/v${ZF_LOG_VERSION}.tar.gz -O /tmp/zf_log.tar.gz && \
-    rm -rf build/zf_log && \
-    mkdir -p build/zf_log && \
-    tar --strip=1 -xf /tmp/zf_log.tar.gz -C build/zf_log && \
-    rm /tmp/zf_log.tar.gz && \
-    cd build/zf_log && \
-    cmake . \
-        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} \
-        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} \
-        -DCMAKE_INSTALL_PREFIX=${ROOT_WORKING_DIR}/install/zf_log/${ZF_LOG_VERSION} && \
-    make install
