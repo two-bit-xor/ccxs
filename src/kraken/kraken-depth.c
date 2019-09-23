951b6dd923ed3e2ded3a793cc4a22cdc43c86e38
xor <48882580+two-bit-xor@users.noreply.github.com>
2019-09-23 22:48:50 +1200

    市场深度 Kraken

--- src/kraken/kraken-depth.c
+++ src/kraken/kraken-depth.c
@@ -5,6 +5,26 @@
 #include "../io/time.h"
 #include "../io/c-json.h"
 
+static long kraken_channel_id = 0;
+
+int
+kraken_parse_order_node(const cJSON *order_book_node, Order *orders) {
+    const cJSON *order_node = NULL;
+
+    int i = 0;
+    cJSON_ArrayForEach(order_node, order_book_node) {
+        cJSON *price = cJSON_GetArrayItem(order_node, 0);
+        cJSON *volume = cJSON_GetArrayItem(order_node, 1);
+
+        if (i < 100 && is_valid_string(price) && is_valid_string(volume)) {
+            Order book = {.price=string_to_double(price->valuestring), .amount=string_to_double(volume->valuestring)};
+            orders[i++] = book;
+        }
+    }
+    orders[i] = EMPTY_ORDER;
+    return i;
+}
+
 OrderBookLevel2 *
 kraken_parse_depth_update(const char *json_string) {
     clock_t t_0 = clock();
@@ -18,32 +38,82 @@ kraken_parse_depth_update(const char *json_string) {
         return NULL;
     }
 
-    const cJSON *channel_name = cJSON_GetObjectItemCaseSensitive(root_node, "channel");
-    const cJSON *type = cJSON_GetObjectItemCaseSensitive(root_node, "event");
-    if (type != NULL && cJSON_IsString(type) && type->valuestring != NULL) {
-        if (strcmp("bts:subscription_succeeded", type->valuestring) == 0) {
-            lwsl_user("%s subscribed: name=\"%s\"\n", __func__, channel_name->valuestring);
-            cJSON_Delete(root_node);
-            return NULL;
-        }
-        if (strcmp("error", type->valuestring) == 0) {
-            lwsl_err("%s error: msg=\"%s\"\n", __func__, json_string);
-            cJSON_Delete(root_node);
-            return NULL;
+    if (cJSON_IsObject(root_node)) {
+        const cJSON *type = cJSON_GetObjectItemCaseSensitive(root_node, "event");
+        if (type != NULL && cJSON_IsString(type) && type->valuestring != NULL) {
+            if (strcmp("heartbeat", type->valuestring) == 0) {
+                lwsl_user("%s heartbeat\n", __func__);
+                cJSON_Delete(root_node);
+                return NULL;
+            }
+            if (strcmp("systemStatus", type->valuestring) == 0) {
+                const cJSON *status = cJSON_GetObjectItemCaseSensitive(root_node, "status");
+                lwsl_user("%s status=\"%s\"\n", __func__, status->valuestring);
+                cJSON_Delete(root_node);
+                return NULL;
+            }
+            if (strcmp("subscriptionStatus", type->valuestring) == 0) {
+                const cJSON *status = cJSON_GetObjectItemCaseSensitive(root_node, "status");
+                const cJSON *pair = cJSON_GetObjectItemCaseSensitive(root_node, "pair");
+                const cJSON *channel = cJSON_GetObjectItemCaseSensitive(root_node, "channelID");
+                kraken_channel_id = channel->valueint;
+                lwsl_user("%s subscription: status=\"%s\", pair=\"%s\", channel=\"%ld\"\n",
+                         __func__, status->valuestring, pair->valuestring, kraken_channel_id);
+                cJSON_Delete(root_node);
+                return NULL;
+            }
         }
     }
+    if (!cJSON_IsArray(root_node)) {
+        cJSON_Delete(root_node);
+        return NULL;
+    }
 
-    const cJSON *data_node = cJSON_GetObjectItemCaseSensitive(root_node, "data");
-    if (data_node == NULL) {
+    int array_size = cJSON_GetArraySize(root_node);
+    int item_index = 0;
+    cJSON *channel = cJSON_GetArrayItem(root_node, item_index++);
+    if (array_size < 4) {
+        cJSON_Delete(root_node);
+        return NULL;
+    }
+    if (channel == NULL || !cJSON_IsNumber(channel) || channel->valueint != kraken_channel_id) {
         cJSON_Delete(root_node);
         return NULL;
     }
 
     OrderBookLevel2 *order_book = malloc(sizeof(OrderBookLevel2));
-    order_book->exchange = strndup("bitstamp", 20);
-    order_book->market_name = strndup("BTC-USD", 20);
+    order_book->exchange = strndup("kraken", 20);
     order_book->time = find_time();
+    order_book->asks_length = 0;
+    order_book->bids_length = 0;
 
+    cJSON *payload_0 = cJSON_GetArrayItem(root_node, item_index++);
+    if (payload_0 != NULL && cJSON_IsObject(payload_0)) {
+        cJSON *asks_snapshot = cJSON_GetObjectItemCaseSensitive(payload_0, "as");
+        if (asks_snapshot != NULL) {
+            order_book->asks_length = kraken_parse_order_node(asks_snapshot, order_book->asks);
+            cJSON *bids_snapshot = cJSON_GetObjectItemCaseSensitive(payload_0, "bs");
+            if (bids_snapshot != NULL) {
+                order_book->bids_length = kraken_parse_order_node(bids_snapshot, order_book->bids);
+            }
+            item_index++;
+        } else {
+            cJSON *asks_update = cJSON_GetObjectItemCaseSensitive(payload_0, "a");
+            if (asks_update != NULL) {
+                order_book->asks_length = kraken_parse_order_node(asks_update, order_book->asks);
+                cJSON *payload_1 = cJSON_GetArrayItem(root_node, item_index++);
+                cJSON *bids_update = cJSON_GetObjectItemCaseSensitive(payload_1, "b");
+                if (bids_update != NULL) {
+                    order_book->bids_length = kraken_parse_order_node(bids_update, order_book->bids);
+                    item_index++;
+                }
+            }
+        }
+    }
+    cJSON *market_node = cJSON_GetArrayItem(root_node, item_index);
+    if (market_node != NULL && cJSON_IsString(market_node)) {
+        order_book->market_name = strndup(market_node->valuestring, 20);
+    }
 
     double t_1 = ((double) clock() - t_0) / CLOCKS_PER_SEC;
 
--- test/kraken/test-kraken-depth.c
+++ test/kraken/test-kraken-depth.c
@@ -11,10 +11,75 @@
 
 static void
 test_kraken_parse_subbed() {
-    char *depth_json = read_file("resources/kraken-subbed.json", 153);
+    char *depth_json = read_file("resources/kraken-subbed.json", 193);
     OrderBookLevel2 *actual = kraken_parse_depth_update(depth_json);
 
     assert_null(actual);
+
+    free(depth_json);
+    orderbook_delete(actual);
+}
+
+static void
+test_kraken_parse_heartbeat() {
+    char *depth_json = read_file("resources/kraken-heartbeat.json", 27);
+    OrderBookLevel2 *actual = kraken_parse_depth_update(depth_json);
+
+    assert_null(actual);
+
+    free(depth_json);
+    orderbook_delete(actual);
+}
+
+static void
+test_kraken_parse_info() {
+    char *depth_json = read_file("resources/kraken-info.json", 113);
+    OrderBookLevel2 *actual = kraken_parse_depth_update(depth_json);
+
+    assert_null(actual);
+
+    free(depth_json);
+    orderbook_delete(actual);
+}
+
+static void
+test_kraken_parse_snapshot() {
+    char *subbed_json = read_file("resources/kraken-subbed.json", 193);
+
+    assert_null(kraken_parse_depth_update(subbed_json));
+    free(subbed_json);
+
+    char *depth_json = read_file("resources/kraken-snapshot.json", 1882);
+    OrderBookLevel2 *actual = kraken_parse_depth_update(depth_json);
+
+    assert_string_equal(actual->exchange, "kraken");
+    assert_string_equal(actual->market_name, "XBT/USD");
+    assert_int_equal(actual->bids_length, 10);
+    assert_int_equal(actual->asks_length, 10);
+    assert_float_equal(actual->asks[0].price, 10161.40000, 0.01);
+    assert_float_equal(actual->asks[0].amount, 5.01437941, 0.01);
+    assert_float_equal(actual->bids[0].price, 10161.30000, 0.01);
+    assert_float_equal(actual->bids[0].amount, 1.60177616, 0.01);
+    free(depth_json);
+    orderbook_delete(actual);
+}
+
+static void
+test_kraken_parse_update() {
+    char *subbed_json = read_file("resources/kraken-subbed.json", 193);
+
+    assert_null(kraken_parse_depth_update(subbed_json));
+    free(subbed_json);
+
+    char *depth_json = read_file("resources/kraken-update.json", 151);
+    OrderBookLevel2 *actual = kraken_parse_depth_update(depth_json);
+
+    assert_string_equal(actual->exchange, "kraken");
+    assert_string_equal(actual->market_name, "XBT/USD");
+    assert_int_equal(actual->bids_length, 0);
+    assert_int_equal(actual->asks_length, 1);
+    assert_float_equal(actual->asks[0].price, 9871.20000, 0.01);
+    assert_float_equal(actual->asks[0].amount, 0.25068544, 0.01);
 
     free(depth_json);
     orderbook_delete(actual);
@@ -27,6 +92,10 @@ main() {
 
     const struct CMUnitTest tests[] = {
             cmocka_unit_test(test_kraken_parse_subbed),
+            cmocka_unit_test(test_kraken_parse_heartbeat),
+            cmocka_unit_test(test_kraken_parse_info),
+            cmocka_unit_test(test_kraken_parse_snapshot),
+            cmocka_unit_test(test_kraken_parse_update),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
